bc8ba7b7aae3b0f360043c17658f2d789caf34f2
Roger-Lo2000 <ss37462609734@gmail.com>
2021-12-21 20:42:30 +0800

    delete main

--- all.sh
+++ all.sh
@@ -6,7 +6,7 @@ time=$(date +%M)
 direct=$(date +%m%d)
 echo $time
 
-if [[ $time == 12 ]];then
+if [[ $time == 13 ]];then
 	gsutil -m cp gs://roaddamagedetect.appspot.com/$direct/*.jpeg ~/Yolov4_RDD_detection/inputs/
 	bash run.sh
 	gsutil -m mv -r ./$direct gs://output-rdd/
